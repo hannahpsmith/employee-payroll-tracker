280f3366258d787d1fde0f3ed65b4a120d350ddc
Hannah Smith <hannahpsmith94@gmail.com>
2024-05-07 10:30:02 -0600

    Edits

--- script.js
+++ script.js
@@ -34,25 +34,16 @@ const collectEmployees = function() {
 const displayAverageSalary = function(employeesArray) {
   let employeeCount = employeesArray.length;
   let totalSalary = 0;
-
   for (let i = 0; i < employeeCount; i++) {
     totalSalary += employeesArray[i].salary;
-    let averageSalary = totalSalary / employeeCount;
-
+    function financial(averageSalary) {
+        return Number.parseFloat(averageSalary).toFixed(2);
+    }
+    let averageSalary = financial(totalSalary / employeeCount);
   };
+  // update $0.00
   let averageSalary = totalSalary / employeeCount;
-  console.log (`The average employee salary between our ${employeeCount} employee(s) is  ${averageSalary}.`);
-
-
-
-  //   console.log ('The average employee salary between our' employeesArray.length 'employees is' (avg));
-  // }
-
-//   for (let salary* salary.length)
-//     avg = (salary * salary.length);
-// console.log("The average employee salary between our" employee.length "employees is" avg);
-  //calculate average salary
-  // for loop to create sum, sum of average salaries divide by array.length
+  console.log (`The average employee salary between our ${employeeCount} employee(s) is ${averageSalary}.`);
 
 
   // TODO: Calculate and display the average salary
@@ -67,12 +58,12 @@ const displayAverageSalary = function(employeesArray) {
   //totalSalary and divide by total number of array items
   //console log
 
-  // i need to take the number of employees, total the salaries, divid by the total number of employees
-
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
+  let randomEmployee = Math.floor(Math.random() * employeesArray.length)
+  console.log(`Congratulations to ${randomEmployee}, our random drawing winner!`)
   // TODO: Select and display a random employee - look up math function to randomize a number, numbers not names --check methods
   // create variable, make it equal to math.random times by array.length -- plug into console log as string using template literal format ${ }
 }
