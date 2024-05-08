bb80c819ef5fa944bca3279d00f656aa7fe854a3
Hannah Smith <hannahpsmith94@gmail.com>
2024-05-08 13:09:40 -0600

    Updated README, edited Average Salary and Random Employee functions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # employee-payroll-tracker
+
+Form to collect employee data, provide the average salary, and select a random employee.
+
+![This Image includes a button to collect employee data, which then calculates the average employee salary and selects a random employee.](./Assets/Website-Capture.PNG)
+
--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
-let employee = {
-  firstName: '', 
-  lastName: '', 
-  salary: ''
-};
-
 let employeesArray = [];
 
+// Collect employee information
 const collectEmployees = function() {
   let addEmployee = true
   while (addEmployee) {
-    employee = {
+    let employee = {
     firstName: '', 
     lastName: '', 
     salary: ''
@@ -31,41 +26,26 @@ const collectEmployees = function() {
     return employeesArray;
 }
 
+
+// Calculate average salary
 const displayAverageSalary = function(employeesArray) {
   let employeeCount = employeesArray.length;
   let totalSalary = 0;
   for (let i = 0; i < employeeCount; i++) {
     totalSalary += employeesArray[i].salary;
-    function financial(averageSalary) {
-        return Number.parseFloat(averageSalary).toFixed(2);
     }
-    let averageSalary = financial(totalSalary / employeeCount);
-  };
-  // update $0.00
-  let averageSalary = totalSalary / employeeCount;
-  console.log (`The average employee salary between our ${employeeCount} employee(s) is ${averageSalary}.`);
-
-
-  // TODO: Calculate and display the average salary
-  //pet example
-  //let totalSalary = 0
-  //index [ 0   ,  1 ,   2 ] 
-  //[Sarah, Joe, {Carli}]
-  //how to access all employees salary? 
-  // for loop (14-Iteration)
-  //employee[i].salary
-  //take ind. salary and add it to the total
-  //totalSalary and divide by total number of array items
-  //console log
-
+    let averageSalary = (totalSalary / employeeCount);
+    let averageSalaryFormattedString = `$${averageSalary.toFixed(2)}`;
+    console.log (`The average employee salary between our ${employeeCount} employee(s) is ${averageSalaryFormattedString}.`);
 }
+
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
-  let randomEmployee = Math.floor(Math.random() * employeesArray.length)
-  console.log(`Congratulations to ${randomEmployee}, our random drawing winner!`)
-  // TODO: Select and display a random employee - look up math function to randomize a number, numbers not names --check methods
-  // create variable, make it equal to math.random times by array.length -- plug into console log as string using template literal format ${ }
+  let randomNumber = Math.floor(Math.random() * employeesArray.length);
+  let randomEmployee = employeesArray[randomNumber];
+  let randomEmployeeFullName = `${randomEmployee.firstName} ${randomEmployee.lastName}`;
+  console.log(`Congratulations to ${randomEmployeeFullName}, our random drawing winner!`)
 }
 
 /*
